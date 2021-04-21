075b65078fd866c3eacb06cea36b0ec9a5d368d1
suret <suretramazanov@gmail.com>
2021-04-21 16:45:49 +0400

    popUpTo, popUpToInclusive & launchSingleTop has been added to nested_nav_graph

--- app/src/main/java/com/suret/taskdesign/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/suret/taskdesign/ui/category/CategoryFragment.kt
@@ -1,13 +1,14 @@
 package com.suret.taskdesign.ui.category
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.suret.taskdesign.R
 import com.suret.taskdesign.adapter.FragmentCategoryRecyclerAdapter
-import com.suret.taskdesign.model.CategoryModel
 import com.suret.taskdesign.listmaker.CategoryModelListMaker
+import com.suret.taskdesign.model.CategoryModel
 import kotlinx.android.synthetic.main.fragment_category.*
 
 
@@ -21,8 +22,10 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
 
         category_toolbar.setNavigationIcon(R.drawable.back_btn)
 
-        category_toolbar.setNavigationOnClickListener {
+
+        category_toolbar.setOnClickListener {
             view.findNavController().navigate(R.id.action_categoryFragment_to_homeFragment)
+
         }
 
         val fragmentCategoryRecyclerAdapter =
@@ -32,5 +35,6 @@ class CategoryFragment : Fragment(R.layout.fragment_category) {
 
 
     }
+
 
 }
